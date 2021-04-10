64cfecd0aa6037a0928a2bbfade11cc07b5bf8b6
NJarosz <63024003+NJarosz@users.noreply.github.com>
2021-04-10 12:22:37 -0600

    Update lcd_counter.py

--- lcd_counter.py
+++ lcd_counter.py
@@ -203,7 +203,7 @@ try:
             empnum = None
             idn = None
             lcd.clear()
-            standby_info_top = f"Part: {part_num}"
+            standby_info_top = f"Part:{part_num}"
             standby_info_btm = f"Cnt:{total_count} Mch:{mach_num}"
             lcd.message(standby_info_top, 1)
             lcd.message(standby_info_btm, 2)
